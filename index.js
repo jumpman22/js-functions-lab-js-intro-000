c5500adebb5f5f0197f982562992f821aa7d8a52
Mila J <icecold240@gmail.com>
2018-02-06 02:05:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-//function happyHolidaysTo(name) {
-//  return "Happy holidays " + ${name} + "!";
-//
+function happyHolidayTo(name) {
+  return "Happy holidays " + ${name} + "!";
